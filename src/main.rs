0686c6384bff07472307fceb2a20305707e27dae
Patrick Rice <patrickrice20@gmail.com>
2020-04-19 18:09:00 -0700

    Accept user input

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use std::fmt;
+use std::io::{self, BufRead};
 
-fn main() {
-    let mut board: Board = Board::new();
-
-    println!("{}", board)
+#[derive(Debug)]
+enum Player {
+    x,
+    o,
 }
 
 #[derive(Debug)]
@@ -24,13 +25,84 @@ impl Board {
 
         Self { board }
     }
+
+    pub fn game_over(&self) -> bool {
+        self.board_full()
+    }
+
+    pub fn make_move(&self, row: usize, col: usize, player: Player) -> () {
+        match self.board[row][col] {
+            "" => {}
+            _ => panic!("Invalid Move: space occupied by {}", self.board[row][col]),
+        }
+    }
+
+    fn board_full(&self) -> bool {
+        for row in &self.board {
+            for c in row {
+                match &c[..] {
+                    "" => return false,
+                    _ => continue,
+                }
+            }
+        }
+        true
+    }
+}
+
+fn get_input() -> Result<(String, String), &'static str> {
+    let stdin = io::stdin();
+    let line1 = stdin.lock().lines().next().unwrap().unwrap();
+    let values: Vec<&str> = line1.split(" ").collect();
+    let (row, col) = (
+        match values.get(0) {
+            Some(x) => x,
+            None => return Err("Invalid input!"),
+        },
+        match values.get(1) {
+            Some(x) => x,
+            None => return Err("Invalid input!"),
+        },
+    );
+    println!("Entered [{:?}, {:?}]: ", row, col);
+    let n = String::from(*row);
+    Ok((String::from(*row), String::from(*col)))
+}
+
+fn main() -> () {
+    let mut board: Board = Board::new();
+    let player = Player::x;
+
+    loop {
+        println!("Turn {:?}: Enter [row, col]: ", player);
+        let input = get_input();
+        let (row, col) = match input {
+            Err(x) => {
+                println!("{:?}", x);
+                continue;
+            }
+            Ok((x, y)) => (x, y),
+        };
+
+        println!("row {}; col {}", row, col);
+
+        match board.game_over() {
+            true => {
+                println!("Game Over!");
+                break;
+            }
+            false => println!("Next Player"),
+        }
+    }
+
+    println!("{}", board)
 }
 
 impl fmt::Display for Board {
     // This trait requires `fmt` with this exact signature.
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         // Write strictly the first element into the supplied output
-        // stream: `f`. Returns `fmt::Result` which indicates whether the
+        // stream: `f`. RePlayers `fmt::Result` which indicates whether the
         // operation succeeded or failed. Note that `write!` uses syntax which
         // is very similar to `println!`.
         let mut out = String::new();
