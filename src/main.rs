028e91cf0b47042a6115c14762b26ad5d284876d
Patrick Rice <patrickrice20@gmail.com>
2020-04-26 11:07:12 -0700

    Less panics

--- src/main.rs
+++ src/main.rs
@@ -3,29 +3,34 @@ use std::fmt;
 use std::io;
 // use std::io::{self, BufRead};
 
-#[derive(Debug)]
+#[derive(Copy, Clone, Debug)]
 enum Player {
-    x,
-    o,
+    X,
+    O,
 }
 
 #[derive(Debug)]
 enum BoardValue {
-    player(Player),
-    empty(),
+    Player(Player),
+    Empty(),
 }
 
 impl fmt::Display for BoardValue {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match &*self {
-            BoardValue::player(v) => write!(f, "{:?}", v),
-            BoardValue::empty() => write!(f, " "),
+            BoardValue::Player(v) => write!(f, "{:?}", v),
+            BoardValue::Empty() => write!(f, " "),
         }
     }
 }
 #[derive(Debug)]
 struct Board {
     board: Vec<Vec<BoardValue>>,
+}
+#[derive(Debug)]
+
+enum BoardError {
+    InvalidMove(std::string::String),
 }
 
 impl Board {
@@ -34,7 +39,7 @@ impl Board {
         for _ in 0..3 {
             let mut row: Vec<BoardValue> = Vec::new();
             for _ in 0..3 {
-                row.push(BoardValue::empty());
+                row.push(BoardValue::Empty());
             }
             board.push(row);
         }
@@ -42,22 +47,32 @@ impl Board {
         Self { board }
     }
 
+    pub fn size(&self) -> usize {
+        self.board.len()
+    }
+
     pub fn game_over(&self) -> bool {
         self.board_full()
     }
 
-    pub fn make_move(&self, row: usize, col: usize, player: Player) -> () {
+    pub fn make_move(&mut self, row: usize, col: usize, player: Player) -> Result<(), BoardError> {
         match self.board[row][col] {
-            BoardValue::empty() => {}
-            _ => panic!("Invalid Move: space occupied by {:?}", self.board[row][col]),
-        }
+            BoardValue::Empty() => self.board[row][col] = BoardValue::Player(Player::from(player)),
+            _ => {
+                return Err(BoardError::InvalidMove(format!(
+                    "Invalid Move: space occupied by {:?}",
+                    self.board[row][col]
+                )))
+            }
+        };
+        Ok(())
     }
 
     fn board_full(&self) -> bool {
         for row in &self.board {
             for cell in row {
                 match cell {
-                    BoardValue::empty() => return false,
+                    BoardValue::Empty() => return false,
                     _ => continue,
                 }
             }
@@ -87,6 +102,7 @@ impl fmt::Display for Board {
 enum InputError {
     IoError(io::Error),
     IndexError(&'static str),
+    RangeError(&'static str),
 }
 
 impl From<io::Error> for InputError {
@@ -95,42 +111,54 @@ impl From<io::Error> for InputError {
     }
 }
 
-fn get_input() -> Result<(u8, u8), InputError> {
+fn get_input(max_size: &usize) -> Result<(usize, usize), InputError> {
     let mut line = String::new();
     io::stdin().read_line(&mut line)?;
 
     let values: Vec<&str> = line.split_whitespace().collect();
-    let values: Vec<u8> = values.iter().filter_map(|x| parse_input(x).ok()).collect();
+    let values: Vec<usize> = values.iter().filter_map(|x| parse_input(x).ok()).collect();
     match values.len() {
         2 => {}
         _ => return Err(InputError::IndexError("Invalid number of valid inputs")),
     }
-    let (row, col): (u8, u8) = (*values.get(0).unwrap(), *values.get(1).unwrap());
+    let (row, col): (usize, usize) = (*values.get(0).unwrap(), *values.get(1).unwrap());
+    for item in [row, col].iter() {
+        let min: usize = 0;
+        if item < &min || item > max_size {
+            return Err(InputError::RangeError("Input not in valid range of board!"));
+        }
+    }
 
     Ok((row, col))
 }
 
-fn parse_input(input: &str) -> Result<u8, core::num::ParseIntError> {
-    Ok(String::from(input).parse::<u8>())?
+fn parse_input(input: &str) -> Result<usize, core::num::ParseIntError> {
+    Ok(String::from(input).parse::<usize>())?
 }
 
 fn main() -> () {
-    let board: Board = Board::new();
-    let player = Player::x;
+    let mut board: Board = Board::new();
+    let mut player = Player::X;
 
     loop {
         println!("{}", board);
-        println!("Turn {:?}: Enter [row, col]: ", player);
-        // let input = get_input();
-        let (row, col) = match get_input() {
+        println!("Turn {:?}: Enter [row, col]: ", &player);
+
+        let (row, col) = match get_input(&board.size()) {
             Err(x) => {
                 println!("{:?}", x);
                 continue;
             }
-            Ok((x, y)) => (x, y),
+            Ok((x, y)) => (x - 1, y - 1),
         };
 
-        println!("row {}; col {}", row, col);
+        match board.make_move(row, col, player) {
+            Err(x) => {
+                println!("{:?}", x);
+                continue;
+            }
+            _ => {}
+        }
 
         match board.game_over() {
             true => {
@@ -139,23 +167,12 @@ fn main() -> () {
             }
             false => println!("Next Player"),
         }
+
+        player = match player {
+            Player::X => Player::O,
+            _ => Player::X,
+        };
     }
 
     println!("{}", board)
 }
-
-// impl fmt::Display for Board {
-//     // This trait requires `fmt` with this exact signature.
-//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-//         // Write strictly the first element into the supplied output
-//         // stream: `f`. RePlayers `fmt::Result` which indicates whether the
-//         // operation succeeded or failed. Note that `write!` uses syntax which
-//         // is very similar to `println!`.
-//         let mut out = String::new();
-//         for row in &self.board {
-//             out.push_str(&format!("{:?}\n", row).to_owned())
-//         }
-
-//         write!(f, "{}", out)
-//     }
-// }
